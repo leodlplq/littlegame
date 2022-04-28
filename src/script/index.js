ee7b3015fe8cc22b765c6a7a8e1d8dd0daab80f9
Leo Delplanque <delplanqueleomail@gmail.com>
2022-04-28 13:59:57 +0200

    :sparkles: add projectile

--- src/script/index.js
+++ src/script/index.js
@@ -2,6 +2,7 @@ let canvas
 let ctx
 let player
 let enemies = []
+let projectiles = []
 let cursor = {
 	x: 0,
 	y: 0,
@@ -13,7 +14,7 @@ window.onload = () => {
 	canvas.height = window.innerHeight
 
 	player = new Player(ctx, canvas, '#ffc800')
-	for (let i = 0; i < 1; i++) {
+	for (let i = 0; i < 9; i++) {
 		enemies.push(new Enemy(ctx, canvas, 'red'))
 	}
 	animate()
@@ -25,7 +26,20 @@ const animate = () => {
 	enemies.forEach((enemey) => {
 		enemey.update()
 	})
+	projectiles.forEach((projectile, index) => {
+		projectile.update()
+		if (
+			projectile.position.x >= canvas.width ||
+			projectile.position.x <= 0 ||
+			projectile.position.y >= canvas.height ||
+			projectile.position.y < 0
+		) {
+			console.log('sorti')
+			projectiles.splice(index, 1)
+		}
+	})
 	player.update()
+
 	movePlayer()
 }
 
@@ -43,9 +57,63 @@ window.addEventListener('mousemove', (e) => {
 	cursor.y = e.y
 })
 
+window.addEventListener('keyup', (e) => {
+	// console.log(e)
+	if ((e.key = ' ')) {
+		console.log('fire')
+		projectiles.push(
+			new Projectile(
+				{
+					position: {
+						x: player.position.x,
+						y: player.position.y,
+					},
+					velocity: {
+						x: 10 * Math.cos(getAngle()),
+						y: 10 * Math.sin(getAngle()),
+					},
+				},
+				ctx
+			)
+		)
+		console.log(projectiles)
+	}
+})
+
+window.addEventListener('click', (e) => {
+	// console.log(e)
+	if ((e.key = ' ')) {
+		console.log('fire')
+		projectiles.push(
+			new Projectile(
+				{
+					position: {
+						x: player.position.x,
+						y: player.position.y,
+					},
+					velocity: {
+						x: 10 * Math.cos(getAngle()),
+						y: 10 * Math.sin(getAngle()),
+					},
+				},
+				ctx
+			)
+		)
+		console.log(projectiles)
+	}
+})
+
 const movePlayer = () => {
 	player.velocity.x = (cursor.x - player.position.x) * 0.1
 	player.velocity.y = (cursor.y - player.position.y) * 0.1
+}
+
+const getAngle = () => {
+	return Math.atan2(
+		cursor.y - player.position.y,
+		cursor.x - player.position.x
+	)
+	// (180 / Math.PI)
 }
 
 class Player {
@@ -67,8 +135,21 @@ class Player {
 
 	draw(x, y) {
 		this.ctx.beginPath()
+
 		this.ctx.arc(x, y, this.radius / 2, 0, Math.PI * 2)
 		this.ctx.fillStyle = this.color
+		this.ctx.fill()
+		this.ctx.closePath()
+
+		this.ctx.beginPath()
+		this.ctx.arc(
+			x + (this.radius + 0) * Math.cos(getAngle()),
+			y + (this.radius + 0) * Math.sin(getAngle()),
+			2,
+			0,
+			Math.PI * 2
+		)
+		this.ctx.fillStyle = 'white'
 		this.ctx.fill()
 		this.ctx.closePath()
 	}
@@ -137,5 +218,36 @@ class Enemy {
 		) {
 			this.velocity.y = -this.velocity.y
 		}
+	}
+}
+
+class Projectile {
+	constructor({ position, velocity }, ctx) {
+		this.position = position
+		this.velocity = velocity
+
+		this.ctx = ctx
+
+		this.radius = 3
+	}
+
+	draw() {
+		this.ctx.beginPath()
+		this.ctx.arc(
+			this.position.x,
+			this.position.y,
+			this.radius,
+			0,
+			Math.PI * 2
+		)
+		this.ctx.fillStyle = 'green'
+		this.ctx.fill()
+		this.ctx.closePath()
+	}
+
+	update() {
+		this.draw()
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
 	}
 }
